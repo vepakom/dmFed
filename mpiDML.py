217b8ccc0f8ca474abf4cb13576db9e54554ee6b
Praneeth <46326477+vepakom@users.noreply.github.com>
2021-07-07 16:58:23 -0400

    Update mpiDML.py

--- mpiDML.py
+++ mpiDML.py
@@ -4,42 +4,28 @@ from mpi4py import MPI t as plt
 import numpy as np
 import matplotlib.pyplot as plt
 import randomMM_WORLD
-#Open communicationk()
+#Open communication()
 comm = MPI.COMM_WORLD nline")
 rank = comm.Get_rank()
-print(str(rank) + ": online")    [ Read 30 lines ]
-^G Get Help  ^O Write Out ^W Where Is  ^K Cut Text  ^J Justify   ^C Cur Pos
+print(str(rank) + ": online")    
+
 if(rank == 0):
     #initialize server variables
 else:
-
     #initialize client variables
 
-if(root == 0):
-        #prints the number of nodes (#clients + 1 server) if(rank == 0): #A comput$
+if(rank == 0):
+        #prints the number of nodes (#clients + 1 server) 
     size = comm.Get_size()
     print(size)
     avgWeights = []
       for i in range(1,size):
            array = np.empty(avgWeights.shape, dtype=np.float32)
            comm.Recv(array,source=i) #server rcvs local weight via MPI
-           avgWeights.append(array) # make this streaming average instead or chang$
-else:
-    #train local model code and create localWeight
-    comm.Send(localWeight,dest=0) #clients send their local weight via MPI
-    if(root == 0):
-        #prints the number of nodes (#clients + 1 server) if(rank == 0): #A comput$
-    size = comm.Get_size()
-    print(size)
-    avgWeights = []
-      for i in range(1,size):
-           array = np.empty(avgWeights.shape, dtype=np.float32)
-           comm.Recv(array,source=i) #server rcvs local weight via MPI
-           avgWeights.append(array) # make this streaming average instead or chang$
+           avgWeights.append(array) # make this streaming average instead or change to use mpi.gather
 else:
     #train local model code and create localWeight
     comm.Send(localWeight,dest=0) #clients send their local weight via MPI
 
-
-avgWeights = comm.bcast(avgWeights, root=0) # server broadcasts the averaged weigh$
-# note any computation now done on avgWeights gets computed by all clients automat$
+avgWeights = comm.bcast(avgWeights, root=0) # server broadcasts the averaged weight
+#note any computation/eqn now done on avgWeights gets computed by all clients automatically
